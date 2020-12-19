9c80f01b223cb83976aacdc7dbee8a964773c5dc
Saurav-Shrivastav <sshrivastav_be19@thapar.edu>
2020-12-19 21:34:29 +0530

    Models for the auctions app

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+admin.site.register(models.Listing)
+admin.site.register(models.Bid)
+admin.site.register(models.Comment)
--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,43 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Listing(models.Model):
+    CATEGORY_CHOICES = (
+        ("fashion", "Fashion"),
+        ("tech", "Technology"),
+        ("elec", "Electronics"),
+        ("home", "Home"),
+        ("pantry", "Pantry"),
+        ("toys", "Toys")
+    )
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    image_url = models.URLField()
+    starting_bid = models.DecimalField(max_digits=19, decimal_places=10)
+    category = models.CharField(
+        max_length=7,
+        choices=CATEGORY_CHOICES
+    )
+
+    def __str__(self):
+        return self.title
+
+
+class Bid(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=19, decimal_places=10)
+
+    def __str__(self):
+        return f"Bid on {self.listing.title} by {self.user.username}"
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    text = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"Comment on {self.listing.title} by {self.user.username}"
